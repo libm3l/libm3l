d850104b659b1b9910825773b74b5ca9570e5513
jka <jka@eaglespeak.(none)>
2012-04-13 01:00:47 -0600

    changed #define variable in tcpip_socket_op.h removed -lssl from Makefile

--- tcpip_socket_op.h
+++ tcpip_socket_op.h
@@ -1,14 +1,6 @@
-#ifndef __CLI_OPEN_SOCKET_H__
-#define __CLI_OPEN_SOCKET_H__
+#ifndef __TCPIP_SOCKET_OP_H__
+#define __TCPIP_SOCKET_OP_H__
 
 int openbindlistensocket(int );
-
-SSL_CTX* init_server_context(void);
-
-SSL_CTX* init_client_context(void);
-
-void load_ssl_certificates(SSL_CTX* , char* , char* );
-
-void show_ssl_certs(SSL*);
 
 #endif
