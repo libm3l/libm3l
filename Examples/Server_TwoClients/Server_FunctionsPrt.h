ea5a6adbc3b48e87b43fc8625856a89499f0fc8d
Adam Jirasek <Adam.Jirasek@gmail.com>
2013-03-18 11:35:01 -0600

    added function propotypes to Server_TwoClients example

---
+++ Examples/Server_TwoClients/Server_FunctionsPrt.h
@@ -0,0 +1,44 @@
+/*
+ *     Copyright (C) 2012  Adam Jirasek
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     contact: libm3l@gmail.com
+ * 
+ */
+
+
+
+/*
+ *     Header file FunctionsPrt.h
+ *
+ *     Date: 2012-05-21
+ * 
+ *
+ *     Modifications:
+ *     Date		Version		Patch number		CLA 
+ *
+ *
+ *     Description
+ *
+ */
+
+#ifndef  __SERVER_FUNCTIONPRT_H__
+#define  __SERVER_FUNCTIONPRT_H__
+
+extern void sig_chld(lmint_t );
+extern lmint_t Fork(void);
+
+#endif
+
