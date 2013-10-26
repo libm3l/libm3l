8011a61c58b7c53b776df3469e8f1623339e1bfe
Adam Jirasek <Adam.Jirasek@gmail.com>
2013-10-26 14:41:50 -0600

    added copyright statement to Set_Default_Parameters

--- Source/Set_Default_Parameters.c
+++ Source/Set_Default_Parameters.c
@@ -1,3 +1,52 @@
+/*
+ *     Copyright (C) 2013  Adam Jirasek
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     contact: libm3l@gmail.com
+ * 
+ */
+
+
+
+
+/*
+ *     Function Set_Default_Parameters.c
+ *
+ *     Date: 2013-10-24
+ * 
+ * 
+ *     Description:
+ * 
+ *
+ *     Input parameters:
+ * 
+ *
+ *     Return value:
+ * 
+ * 
+ *
+ *     Modifications:
+ *     Date		Version		Patch number		CLA 
+ *
+ *
+ *     Description
+ * 
+ */
+
+
+
 #include "format_type.h"
 #include "internal_format_type.h"
 #include "Set_Default_Parameters.h"
