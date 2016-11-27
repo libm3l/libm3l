5a133e6822394813113730c8a500ad1dc3a8fce0
Adam Jirasek <Adam.Jirasek@gmail.com>
2016-11-27 14:01:06 -0700

    move to data_util directory

---
+++ Source/data_util/libm3l.h
@@ -0,0 +1,68 @@
+/*
+ *     Copyright (C) 2014  Adam Jirasek
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     contact: libm3l@gmail.com
+ * 
+ */
+
+
+
+
+
+#ifndef   __LIBM3L_H__
+#define   __LIBM3L_H__
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/libm3l_header.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/format_type.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/internal_format_type.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Add.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/add_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Cat.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/cat_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Check_EOFbuff.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Cp.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/cp_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Detach.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/detach_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Find.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/find_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/FunctionsPrt.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/IO.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/IO_types.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/ListDataOperations.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Ln.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/ln_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Locate.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/locate_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Mklist.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Mount.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Mv.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/mv_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/NumberConversion.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/ReadBDescriptor.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/ReadDescriptor.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/ReadSocket.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Rm.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/rm_list.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Set_Default_Parameters.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/tcpip_socket_IOop.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/tcpip_socket_MNTop.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/tcpip_socket_op.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/udf_rm.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Umount.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/Write2Socket.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/WriteBData.h"
+#include  "/home/jka/Cprograms/Sources/libm3l/Source/data_util/WriteData.h"
+#endif
